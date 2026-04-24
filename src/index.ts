ac1cd207f4196c6f0b21f2d03eacb9f41fd9d610
Yusuke Kamiyamane <y@y14e.com>
2026-04-24 20:34:56 +0900

    v0.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /**
- * sig8
+ * sigggnal
  * High-performance async machinery powered by AbortSignal.
  * Supports cancellation, timeouts, retries, and concurrency control.
  *
@@ -7,7 +7,7 @@
  * @author Yusuke Kamiyamane
  * @license MIT
  * @copyright Copyright (c) 2026 Yusuke Kamiyamane
- * @see {@link https://github.com/y14e/sig8}
+ * @see {@link https://github.com/y14e/sigggnal}
  */
 
 // concurrent
